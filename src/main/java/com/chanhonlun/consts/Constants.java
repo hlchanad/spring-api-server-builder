6d254edb72324b77d0e1057505172578d0ad2d60
hlchanad <hlchanad@stu.ust.hk>
2018-04-14 15:41:34 +0800

    PropertiesUtil for getting property in .properties file

---
+++ src/main/java/com/chanhonlun/consts/Constants.java
@@ -0,0 +1,7 @@
+package com.chanhonlun.consts;
+
+public class Constants {
+
+    public static final String PROP_FILE_PATH               = "application.properties";
+    
+}
---
+++ src/main/java/com/chanhonlun/utils/PropertiesUtil.java
@@ -0,0 +1,34 @@
+package com.chanhonlun.utils;
+
+import com.chanhonlun.consts.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
+
+    private static Properties properties;
+
+    public static void init() throws IOException {
+
+        logger.info("start");
+
+        properties = new Properties();
+
+        InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(Constants.PROP_FILE_PATH);
+
+        properties.load(input); // load a properties file
+
+        logger.info("end");
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key, "property not found");
+    }
+
+}
