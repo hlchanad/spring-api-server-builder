55338273be56a19c27e2a8021636c42336791e2a
hlchanad <hlchanad@stu.ust.hk>
2018-05-29 22:17:54 +0800

    rename twigs

--- src/main/java/com/chanhonlun/builder/consts/TemplatePathConstants.java
+++ src/main/java/com/chanhonlun/builder/consts/TemplatePathConstants.java
@@ -3,11 +3,11 @@ package com.chanhonlun.builder.consts;
 public class TemplatePathConstants {
 
     public static final String RESOURCES_BUILD_DOT_GRADLE_PATH          = "twig/build.gradle.twig";
-    public static final String RESOURCES_POJO_PATH                      = "twig/pojo.twig";
-    public static final String RESOURCES_BASE_REPO_PATH                 = "twig/baseRepository.twig";
-    public static final String RESOURCES_BASE_DETAIL_REPO_PATH          = "twig/baseDetailRepository.twig";
-    public static final String RESOURCES_REPO_PATH                      = "twig/repository.twig";
-    public static final String RESOURCES_DETAIL_REPO_PATH               = "twig/detailRepository.twig";
-    public static final String RESOURCES_HELLO_WORLD_PATH               = "twig/HelloWorldController.twig";
+    public static final String RESOURCES_POJO_PATH                      = "twig/pojos/pojo.twig";
+    public static final String RESOURCES_BASE_REPO_PATH                 = "twig/repositories/baseRepository.twig";
+    public static final String RESOURCES_BASE_DETAIL_REPO_PATH          = "twig/repositories/baseDetailRepository.twig";
+    public static final String RESOURCES_REPO_PATH                      = "twig/repositories/repository.twig";
+    public static final String RESOURCES_DETAIL_REPO_PATH               = "twig/repositories/detailRepository.twig";
+    public static final String RESOURCES_HELLO_WORLD_PATH               = "twig/controllers/HelloWorldController.twig";
     public static final String RESOURCES_LOGBACK_XML_PATH               = "twig/resources/logback.xml.twig";
 }
